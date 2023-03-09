35211e73dc142ac9a860f347d23b5dcd3326cc2c
rrobert02 <robert.rotaru18@outlook.com>
2023-03-09 16:11:39 +0200

    Add project files.

---
+++ Tap.Solid.Ocp/BankAccount.cs
@@ -0,0 +1,31 @@
+﻿namespace Tap.Solid.Ocp
+{
+    public class BankAccount
+    {
+        public decimal AccountBalance { get; private set; }
+        public AccountType AccountType { get; private set; }
+
+        public BankAccount(decimal initialBalance, AccountType accountType)
+        {
+            AccountBalance = initialBalance;
+            AccountType = accountType;
+        }
+
+        public void ExtractMoney(decimal value)
+        {
+            var commision = 0m;
+
+            if(AccountType == AccountType.Regular)
+            {
+                commision = value * 0.04m;
+            }
+
+            if (AccountType == AccountType.Gold)
+            {
+                commision = value * 0.02m;
+            }
+
+            AccountBalance = AccountBalance - (value + commision);
+        }
+    }
+}
---
+++ Tap.Solid.Srp/Student.cs
@@ -0,0 +1,42 @@
+﻿using Dapper;
+using Microsoft.Data.Sqlite;
+using System.Net.Mail;
+
+namespace Tap.Solid.Srp
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string SocialSecurityNumber { get; set; }
+
+        public string AddStudent()
+        {
+            if (!Email.Contains("@uaic.ro"))
+                return "Invalid email. Please use your uaic email!";
+
+            if (SocialSecurityNumber.Length != 13)
+                return "Invalid social security number!";
+
+            var sqlCommand = "INSERT INTO students (Name, Email, SocialSecurityNumber) values (@Name, @Email, @SocialSecurityNumber)";
+            using (var connection = new SqliteConnection("Data Source=:memory:"))
+            {
+                var affectedRows = connection.Execute(sqlCommand);
+            }
+
+            var mail = new MailMessage("demo@solid.principles.srp", Email);
+            var client = new SmtpClient
+            {
+                Port = 25,
+                Host = "smtp.demo.exercise"
+            };
+
+            mail.Subject = "Welcome to the lab!";
+            mail.Body = $"Hey {Name}! Glad to have you!";
+            client.Send(mail);
+
+            return "Student enroled!";
+        }
+    }
+}
